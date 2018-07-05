a1b7694b7b174be70cf6b7183b5eee0acfb9b5ff
Keir Mcluskey <keir.mcluskey@hotmail.co.uk>
2018-07-05 23:40:53 +1000

    Removes sdk man download url to variable and swaps volume up/down for mute

--- install-tools.sh
+++ install-tools.sh
@@ -1,9 +1,11 @@
 #!/bin/bash -e
 
+SDK_MAN_URL=https://get.sdkman.io
+
 # Brew Dependancies
 brew install zsh zsh-completions 
 brew install git node bower 
 
 # SDK Man
-curl -s "https://get.sdkman.io" | bash
+curl -s "$SDK_MAN_URL" | bash
 echo 'source "$HOME/.sdkman/bin/sdkman-init.sh"' >> ~/.zshrc
