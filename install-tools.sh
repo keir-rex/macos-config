824fd4063f0c8f054364ad12e9c408a61089c15d
Keir Mcluskey <keir@Keirs-MacBook-Pro.local>
2019-08-19 08:13:34 +1000

    Some updates

--- install-tools.sh
+++ install-tools.sh
@@ -1,18 +1,38 @@
-#!/bin/bash -e
+#!/usr/bin/env bash
 
-SDK_MAN_URL=https://get.sdkman.io
+if !hash brew 2>/dev/null; then
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
 
-# Brew Dependancies
-brew install zsh zsh-completions 
-brew install git node bower 
+# Install CLI tools
+brew install git coreutils findutils gnu-tar gnu-sed gawk gnutls gnu-indent gnu-getopt grep tree ag openssh gpg
+brew install vim --with-override-system-vi screen
 
-# SDK Man
-curl -s "$SDK_MAN_URL" | bash
-echo 'source "$HOME/.sdkman/bin/sdkman-init.sh"' >> ~/.zshrc
+# Install newer shells
+brew install bash zsh zsh-completions
 
-# Global NPM Packages
-npm install -g nd
+# Install programming utils
+brew install go node
+brew cask install iterm2
 
-# Spectacle
-curl https://s3.amazonaws.com/spectacle/downloads/Spectacle+1.2.zip > ~/Downloads/spectacle.zip
-cp -r ~/Downloads/Spectacle.app /Applications
+# Install cloud-utils
+brew install awscli google-cloud-sdk 
+brew cask install minikube minishift kubernetes-helm
+
+# Install usability utilities
+brew cask install spectacle karabiner-elements bettertouchtool
+
+# Install full GUI applications
+brew cask install spotify vscodium google-chrome 1password
+
+brew cleanup
+
+# Copy spectacle configuration to appropriate directory
+mv ./dotfiles/Shortcuts.json ~/Library/Application Support\ Spectacle/Shortcuts.json
+
+# Change to zsh and install oh-my-zsh
+chsh -s $(which zsh)
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Add this to ~/.zshrc
+# fpath=(/usr/local/share/zsh-completions $fpath)
